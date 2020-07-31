a153a4a3f39853513cce00d8d940fea6b0ddb125
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-07-31 19:13:05 +0430

    get.py

---
+++ get.py
@@ -0,0 +1,15 @@
+print '''\033[92m
+print "coding"
+print"by   "
+print"M0bin-Dan"
+print"my chanell:@mr_server inrubik"
+print"my canell in telegram:@MONSTER_SECURITY"
+print'''
+import urllib
+url = raw_input("enter your target: ")
+server = urllib.urlopen(url)
+for key .value in server.headers.items():
+
+    print ""
+    print key+':'+value
+    print ""
