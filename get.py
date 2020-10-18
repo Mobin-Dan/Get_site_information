9f58affa29c27c9049b6ee59e8dca9d813524eff
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-10-18 21:57:59 +0330

    Update get.py

--- get.py
+++ get.py
@@ -1,11 +1,10 @@
+import urllib
 print '''\033[92m
 print "coding"
 print"by   "
 print"M0bin-Dan"
-print"my chanell:@mr_server inrubik"
-print"my canell in telegram:@MONSTER_SECURITY"
+print"follow me:mr_mobin_dan in insta"
 print'''
-import urllib
 url = raw_input("enter your target: ")
 server = urllib.urlopen(url)
 for key .value in server.headers.items():
